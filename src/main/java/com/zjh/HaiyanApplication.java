2e4d8053b8f1e2d7a5b190e5d8f16c4727a8378c
bbs6280 <bbs6280@qq.com>
2018-04-18 17:39:33 +0800

    创建项目

---
+++ src/main/java/com/zjh/HaiyanApplication.java
@@ -0,0 +1,12 @@
+package com.zjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiyanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaiyanApplication.class, args);
+    }
+}
